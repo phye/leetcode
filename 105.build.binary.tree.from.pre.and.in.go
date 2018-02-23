7bc516d186091cfac8121c23031f54a9dacd5944
Philip Ye <phye@cisco.com>
2018-02-23 16:46:33 +0800

    Add tree built via inorder and postorder

--- 105.build.binary.tree.from.pre.and.in.go
+++ 105.build.binary.tree.from.pre.and.in.go
@@ -2,17 +2,10 @@ package main
 
 import (
 	"fmt"
+	base "github.com/phye/algobase"
 )
 
-type (
-	TreeNode struct {
-		Val   int
-		Left  *TreeNode
-		Right *TreeNode
-	}
-)
-
-func buildTree(preorder []int, inorder []int) *TreeNode {
+func buildTree(preorder []int, inorder []int) *base.TreeNode {
 	if len(preorder) == 0 {
 		return nil
 	}
@@ -20,10 +13,25 @@ func buildTree(preorder []int, inorder []int) *TreeNode {
 	for i := 0; i < len(inorder); i++ {
 		m[inorder[i]] = i
 	}
-	root := &TreeNode{preorder[0], nil, nil}
+	root := &base.TreeNode{preorder[0], nil, nil}
 	idx := m[root.Val]
 	root.Left = buildTree(preorder[1:idx+1], inorder[:idx])
 	root.Right = buildTree(preorder[idx+1:], inorder[idx+1:])
+	return root
+}
+
+func buildTreeFromInAndPost(inorder []int, postorder []int) *base.TreeNode {
+	if len(postorder) == 0 {
+		return nil
+	}
+	m := make(map[int]int)
+	for k, v := range inorder {
+		m[v] = k
+	}
+	root := &base.TreeNode{postorder[len(postorder)-1], nil, nil}
+	idx := m[root.Val]
+	root.Left = buildTreeFromInAndPost(inorder[:idx], postorder[:idx])
+	root.Right = buildTreeFromInAndPost(inorder[idx+1:], postorder[idx:len(postorder)-1])
 	return root
 }
 
@@ -32,4 +40,10 @@ func main() {
 		[]int{33, 27, 29, 3, 20, 30, 8, 39, 28},
 		[]int{29, 27, 20, 3, 33, 8, 39, 30, 28})
 	fmt.Printf("%v\n", bt.Val)
+
+	bt2 := buildTreeFromInAndPost(
+		[]int{9, 3, 15, 20, 7},
+		[]int{9, 15, 7, 20, 3},
+	)
+	fmt.Printf("%v\n", bt2.Val)
 }
